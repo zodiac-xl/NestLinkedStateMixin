e0651723218823a029f730856c08ca774c243b79
吴小龙 <wuxiaolong@meituan.com>
2015-12-09 18:48:34 +0800

    绑定的state状态 数组 stateArr 长度扩张5到8

--- NestLinkedStateMixin.js
+++ NestLinkedStateMixin.js
@@ -133,6 +133,15 @@ var NestLinkedStateMixin = {
                     case 5:
                         newState[stateArr[0]][stateArr[1]][stateArr[2]][stateArr[3]][stateArr[4]] = newValue;
                         break;
+                    case 6:
+                        newState[stateArr[0]][stateArr[1]][stateArr[2]][stateArr[3]][stateArr[4]][stateArr[5]] = newValue;
+                        break;
+                    case 7:
+                        newState[stateArr[0]][stateArr[1]][stateArr[2]][stateArr[3]][stateArr[4]][stateArr[5]][stateArr[6]] = newValue;
+                        break;
+                    case 8:
+                        newState[stateArr[0]][stateArr[1]][stateArr[2]][stateArr[3]][stateArr[4]][stateArr[6]][stateArr[6]][stateArr[8]] = newValue;
+                        break;
                 }
                 context.setState(newState);
             }
